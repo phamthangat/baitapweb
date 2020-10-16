df5fcb817be739b134de35c3d57069c2247e1a47
phamthangat <44996026+phamthangat@users.noreply.github.com>
2020-10-16 14:37:27 +0700

    Add files via upload

---
+++ src/main/java/com/nico/store/store/StoreApplication.java
@@ -0,0 +1,18 @@
+package com.nico.store.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@SpringBootApplication
+@EnableCaching
+@EnableScheduling
+public class StoreApplication  {	
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
